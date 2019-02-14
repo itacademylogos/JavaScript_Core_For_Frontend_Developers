18f3582fa856f4f121025ce0b61b4db927557d3d
LogosItAcademy <Tester99037@gmail.com>
2019-02-15 00:45:45 +0200

    added code example from lesson-20

--- lesson-19/selector.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function () {
-    $('#tab').css('background-color', 'yellow');
-
-    $('tr:even').css('background-color', 'red');
-
-    $('tr:last').css('background-color', 'green');
-
-    $('tr:first').css('background-color', 'blue');
-});
